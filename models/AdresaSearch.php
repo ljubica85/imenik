de1040b42088762320ee51b43a9a35d400be6021
ljubica85 <ljubica.aleksic85@gmail.com>
2018-04-13 20:58:42 +0200

    modified AdresaSearch.php

--- models/AdresaSearch.php
+++ models/AdresaSearch.php
@@ -66,4 +66,23 @@ class AdresaSearch extends Adresa
 
         return $dataProvider;
     }
+	
+	public function prveDveAdrese($params){
+			$query = Adresa::find()->orderBy('id');
+			$dataProvider = new ActiveDataProvider([
+			'query'=>$query,
+			'pagination'=>[
+				'pageSize'=> 2
+			],
+			]);
+			
+			$this->load($params);
+			
+			if(!$this->validate()){
+				// uncomment the following line if you do not want to return any records when validation fails
+				// $query->where('0=1');
+				return $dataProvider;
+			}
+		return $dataProvider;
+	}
 }
